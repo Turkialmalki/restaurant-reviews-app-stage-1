060b87f2cfe3eaa66a1e108896767d25a0a41b6f
Steve Prager <stephenprager92@gmail.com>
2018-08-28 22:03:30 -0400

    Reactivate SW

--- service_worker.js
+++ service_worker.js
@@ -6,53 +6,53 @@
 
 */
 
-// const cacheName = 'restaurant-reviews-app-v1';
+const cacheName = 'restaurant-reviews-app-v1';
 
-// // Once install phase complete, add all URLs to the cache
-// self.addEventListener('install', function(event) {
-//   console.log('service worker installing');
+// Once install phase complete, add all URLs to the cache
+self.addEventListener('install', function(event) {
+  console.log('service worker installing');
 
-//   const cacheURLs = [
-//     '/',
-//     '/index.html',
-//     '/restaurant.html',
-//     '/css/styles.css',
-//     '/css/responsive-index.css',
-//     '/css/responsive-rest.css',
-//     '/js/dbhelper.js',
-//     '/js/main.js',
-//     '/js/restaurant_info.js',
-//     '/data/restaurants.json',
-//     '/img/1.jpg',
-//     '/img/2.jpg',
-//     '/img/3.jpg',
-//     '/img/4.jpg',
-//     '/img/5.jpg',
-//     '/img/6.jpg',
-//     '/img/7.jpg',
-//     '/img/8.jpg',
-//     '/img/9.jpg',
-//     '/img/10.jpg'
-//   ];
+  const cacheURLs = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/css/responsive-index.css',
+    '/css/responsive-rest.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+  ];
 
-//   event.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       console.log('cache time');
-//       return cache.addAll(cacheURLs);
-//     })
-//   );
-// });
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('cache time');
+      return cache.addAll(cacheURLs);
+    })
+  );
+});
 
 
-// // Respond to fetch requests with a page from the cache. 
-// // If there's nothing in the cache, go to the network
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
+// Respond to fetch requests with a page from the cache. 
+// If there's nothing in the cache, go to the network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
